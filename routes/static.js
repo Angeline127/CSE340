2ae8e1515e7fb06d103a7d069fa5bba84c55a8c4
Angeline127 <angenyepen@gmail.com>
2024-01-12 22:47:49 -0800

    application

--- routes/static.js
+++ routes/static.js
@@ -8,6 +8,14 @@ router.use("/css", express.static(__dirname + "public/css"));
 router.use("/js", express.static(__dirname + "public/js"));
 router.use("/images", express.static(__dirname + "public/images"));
 
+
+router.get("/", (req, res)=>{
+    res.render("partials/index",{title:'home'})
+  });
+
+
+
+
 module.exports = router;
 
 
